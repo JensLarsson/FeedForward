2fa50696d803678480f4a61d72549c060f2f2e97
Jens Larsson <42602418+PretentiousPanda@users.noreply.github.com>
2021-03-02 09:45:01 +0100

    c

---
+++ Assets/slökdjgölskdj.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class slökdjgölskdj : MonoBehaviour
+{
+    Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = Camera.main;
+    }
+
+    bool b;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            b = !b;
+            camera.backgroundColor = b ? Color.red : Color.blue;
+        }
+    }
+}
